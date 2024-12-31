24cd7fe2988b9af11b69be3728e181fc7b0dae9e
MaharsheeKaria <maharshee.karia@gmail.com>
2024-12-31 12:40:53 -0500

    fix: update production basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 // const isProd = process.env.NODE_ENV === 'production'
-// const basePath = process.env.NODE_ENV === 'production' 
-//   ? process.env.NEXT_PUBLIC_BASE_PATH || '/hti-website' 
-//   : '';
+const basePath = process.env.NODE_ENV === 'production' 
+  ? process.env.NEXT_PUBLIC_BASE_PATH || '/hti-website' 
+  : '';
 
 const nextConfig = {
-    basePath: '',
+    basePath: basePath,
     // basePath: isProd ? '/hti-website' : '',
     output: 'export',
     distDir: 'dist',
